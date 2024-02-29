67f87433563518bae6d69524206bb225d9010ee6
Admin <admin@Nare-Preezma.local>
2024-02-29 22:23:51 +0400

    add movie player page

--- src/components/movie-player/index.tsx
+++ src/components/movie-player/index.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const MoviePlayer = () => {
-  return <div>MoviePlayer</div>;
+  const location = useLocation();
+  const movie = location.state.movie;
+
+  return (
+    <div className="movie-page">
+      <div className="movie-media">
+        <video controls autoPlay>
+          <source src={movie.videoUrl} type="video/mp4" />
+        </video>
+      </div>
+      <div className="movie-details">
+        <h2>{movie.title}</h2>
+        <p>Description: {movie.description}</p>
+        <p>Year: {movie.year}</p>
+        <p>Country: {movie.country}</p>
+        <p>Rating: {movie.rating}</p>
+        <p>Genres: {movie.genres.join(", ")}</p>
+        <p>Actors: {movie.actors.join(", ")}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MoviePlayer;
--- src/components/single-movie/index.tsx
+++ src/components/single-movie/index.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from "react";
 import { Movie } from "../../type";
+import { useNavigate } from "react-router-dom";
 
 type MoviePropTypes = {
   movie: Movie;
 };
 
 const SingleMovie: FC<MoviePropTypes> = ({ movie }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/movie", { state: { movie } });
+  };
   return (
-    <div className="single-movie">
+    <div className="single-movie" onClick={handleClick}>
       <img src={movie.imageUrl} alt={movie.title} />
       <div className="movie-info">
         <h2>{movie.title}</h2>
